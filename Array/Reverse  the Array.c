b223cbfe4f646ebe0db7b8eea3a751575edcc11c
MAYANK KUMAR SINGH <64895688+kumarmayank29@users.noreply.github.com>
2021-04-21 09:32:15 +0530

    Update Reverse  the Array.c

--- Array/Reverse  the Array.c
+++ Array/Reverse  the Array.c
@@ -1,32 +1,49 @@
-#include <stdio.h>
-#include<stdlib.h>
+#include <iostream>
+using namespace std;
+
+int* ReverseArr(int *p,int n)
+{
+    int i=0;
+    int j=n-1;
+    while(i<j)
+    {
+        int temp=p[i];
+            p[i]=p[j];
+            p[j]=temp;
+            i++;
+            j--;
+            if(i==j)
+            {
+                break;
+            }
+    }
+    return p;
+}
 
 int main() {
-   
- 
-     int arr[1000];
-     int N;
-     
-       scanf("%d",&N);
-       for(int i=0;i<N;i++)
-       {
-           scanf("%d",&arr[i]);
-       }
-       int i=0;
-       int j=N-1;
-       while(i<=j)
-       {
-           int temp=arr[i];
-           arr[i]=arr[j];
-           arr[j]=temp;
-           i++;
-           j--;
-           
-       }
-          for(int i=0;i<N;i++)
-       {
-           printf("%d ",arr[i]);
-       }
-       
-   }
-
+    int n;
+    cout<<"Reversed Array \n";
+    cin>>n;
+    int *p=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>p[i];
+    }
+    ReverseArr(p,n);
+    
+    for(int i=0;i<n;i++)
+    {
+        cout<<p[i]<<" ";
+        
+        
+        
+    }
+    //Increase Each element by 1
+    // cout<<"\n\n";
+    
+    //  for(int i=0;i<n;i++)
+    // {
+    //     cout<<p[i]+1<<" ";
+        
+    // }
+}
